677bfbad544884d12fe8e317bedad27e0943a6c4
Mike Schwall <mikschwall@gmail.com>
2025-06-05 09:57:50 -0500

    add redux toolkit

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { createDefaultPreset } = require("ts-jest/presets")
-
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
