f58a85e3746b2af1b8be20561b3764964aaf60fa
Mike Schwall <mikschwall@gmail.com>
2025-06-19 09:15:38 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import Todolist from './Todolist';
 
 const App:React.FC = () => {
     return (
         <>
         <CssBaseline />
             <div style={{marginLeft:"20px"}}>
-                <Todolist/>
+                test
             </div>
         </>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // ✅ new import for React 18+
 import App from './components/App';
 import { Provider } from 'react-redux';
-import combineReducers from './reducers';
 import thunk from 'redux-thunk';
 import { createStore,applyMiddleware } from 'redux';
 
-const store = createStore(combineReducers,applyMiddleware(thunk));
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement); // ✅ React 18+
   root.render(
-     <Provider store={store}>
-      <App/>
-     </Provider>
+     <App/>
   );
 }
