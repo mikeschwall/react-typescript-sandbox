5956b80907b6e91ba68052e3cc153a8ef999f4b7
Mike Schwall <mikschwall@gmail.com>
2025-07-17 08:08:45 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import TodoList from './TodoList';
-import SongList from './SongList';
+
 
 const App:React.FC = () => {
 
@@ -9,9 +8,7 @@ const App:React.FC = () => {
         <>
         <CssBaseline />
             <main style={{marginLeft:"20px"}}>
-                <SongList/>
-                <hr/>
-                <TodoList/>
+                test
             </main>
         </>
     )
