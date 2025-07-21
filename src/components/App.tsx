d7eae51b4e31f851cab3b132247e12e5a0796191
Mike Schwall <mikschwall@gmail.com>
2025-07-21 12:44:05 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,15 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
 import TodoList from './TodoList';
-import { withAuth } from './Hoc';
-import AuthContext from '../context/store';
-import SongList from './SongList';
 
-const EnhancedTodo = withAuth(TodoList);
 
 const App:React.FC = () => {
-
-    const {logStatus,logHandler} = useContext(AuthContext);
 
     return (
         <>
         <CssBaseline />
             <div style={{marginLeft:"20px"}}>
-                <header>
-                    <button onClick={() => logHandler()}>{logStatus === true ? "log out" : "log in"}</button>
-                </header>
-                <main>
-                    
-                    <SongList/>
-                    <hr/>
-                    <EnhancedTodo/>
-                    
-                </main>
+                <TodoList/>
             </div>
         </>
     )
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import { useFetchTodosQuery, useSendPostMutation } from '../store/apis/Todoapi';
-
-interface Todo {
-    id:number;
-    title:string;
-    completed:boolean;
-}
+import React, { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../reducers';
+import { ThunkDispatch } from 'redux-thunk';
+import { addTodo, deleteTodo, fetchTodos, Todo, TodoAction } from '../actions';
+import MyForm from './MyForm';
 
 const TodoList:React.FC = () => {
 
-    const {data} = useFetchTodosQuery(null);
-    const [sendPost,results] = useSendPostMutation();
-    const {isLoading, isSuccess} = results;
-    
-    const handlePost = () => {
-        console.info(results);
-        sendPost({
-            title: 'foo99',
-            body: 'bar99',
-            userId: 199
-        })
+    const todos = useSelector((state:AppState) => state.todos);
+    const dispatch:ThunkDispatch<AppState,void,TodoAction> = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchTodos());
+    },[dispatch]);
+
+    const handleDelete = (n:number) => {
+        dispatch(deleteTodo(n));
     }
 
-    return (
-        <div>
-            <h2>Todos</h2>
-            <button onClick={() => handlePost()} disabled={isLoading}>{isLoading ? "loading" : "send post"}</button><br/>
-            {data && data.slice(0,9).map((item:Todo) => <li key={item.title}>{item.title}</li>)}
-        </div>
-    )
+    const handleFormData = useCallback((data:string) => {
+        console.log(data);
+         dispatch(addTodo({id:Math.random(),title:data,completed:true}))
+    },[dispatch]);
+
+    return <>
+        <h2>Todos</h2>
+        <MyForm getdata={handleFormData}/>
+        {todos && todos.slice(0,10).map(item => <li onClick={() => handleDelete(item.id)} key={item.id}>{item.title}</li>)}
+    </>
 }
 
 export default TodoList;
