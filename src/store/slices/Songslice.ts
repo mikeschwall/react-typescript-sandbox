90a366031ab9a25f5723f0989976c600a0f15f53
Mike Schwall <mikschwall@gmail.com>
2025-08-10 15:17:20 -0500

    feature-aug10

---
+++ src/store/slices/Songslice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { reset } from "../../actions";
+
+export interface Song {
+    title: string;
+}
+
+const initialState = [
+    {title:"song 1"},
+    {title:"song 2"},
+    {title:"song 3"},
+    {title:"song 4"},
+    {title:"song 5"}
+]
+
+export const songSlice = createSlice({
+    name:"songs",
+    initialState,
+    reducers: {
+        addSong(state,action:PayloadAction<Song>) {
+            state.push(action.payload);
+        },
+        removeSong(state,action:PayloadAction<string>) {
+            const index = state.findIndex(p => p.title === action.payload);
+            state.splice(index,1);
+        }
+    },
+    extraReducers(builder) {
+        builder.addCase(reset,() => {
+            return [];
+        })
+    }
+})
+
+export const {addSong, removeSong} = songSlice.actions;
