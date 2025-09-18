0f8cc52b0ff787784892a7d79ccdde34a1b0e7c9
Mike Schwall <mikschwall@gmail.com>
2025-09-18 08:49:47 -0500

    add feature

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { todoApi } from "./apis/Todoapi";
+import { songSlice } from "./slices/Songslice";
+
+export const store = configureStore({
+    reducer: {
+        [todoApi.reducerPath]: todoApi.reducer,
+        [songSlice.name]: songSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(todoApi.middleware)
+    }
+})
+
+console.log(store.getState());
+
+export type RootType = ReturnType<typeof store.getState>;
