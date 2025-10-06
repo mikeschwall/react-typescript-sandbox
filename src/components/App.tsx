f62a245b7a3fe8ca68548a15b2d26acb346068ee
Mike Schwall <mikschwall@gmail.com>
2025-10-06 08:32:16 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,12 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
-import MyButton from './MyButton';
-import AuthContext from '../store/context';
-import WithAuth from './Hoc';
 
 
 const App:React.FC = () => {
-
-    const {logStatus,logHandler} = useContext(AuthContext);
-
-    const EnhancedButton = WithAuth(MyButton);
-
-    const handleClick = () => {
-        alert("test")
-    }
    
     return (
         <>
-        <CssBaseline />
-            <div style={{marginLeft:"20px"}}>
-                <button onClick={() => logHandler()}>{logStatus === true ? "log out" : "log in"}</button>
-                <h2>Test</h2>
-                <EnhancedButton changeLabel={() => handleClick()}>alert</EnhancedButton>
-            </div>
+        test
         </>
     )
 }
