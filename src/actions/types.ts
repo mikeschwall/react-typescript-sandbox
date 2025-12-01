3927bbeeea82bcb2a43732d7cfc74fef0e671b3b
Mike Schwall <mikschwall@gmail.com>
2025-12-01 08:10:24 -0600

    feature commit

--- src/actions/types.ts
+++
@@ -1,6 +0,0 @@
-export enum actionTypes {
-    FETCH_TODOS,
-    DELETE_TODO,
-    GET_MESSAGE,
-    ADD_TODO
-}
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
-import Todolist from './Todolist';
 
 const App:React.FC = () => {
    
     return (
         <>
-       <Todolist/>
+       test
         </>
     )
 }
