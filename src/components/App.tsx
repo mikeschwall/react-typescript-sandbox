a4054cd51d18fc2dfbf7e4e6db814193276de468
Mike Schwall <mikschwall@gmail.com>
2025-12-16 08:29:55 -0600

    feature commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
-import TodoList from './Todolist';
 
 const App:React.FC = () => {
    
     return (
         <>
-        <TodoList/>
+        test
         </>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,11 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore,applyMiddleware } from 'redux';
-import combineReducers from './reducers';
-const store = createStore(combineReducers,applyMiddleware(thunk))
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement); // ✅ React 18+
   root.render(
-    <Provider store={store}>
-      <App/>
-    </Provider>
+    <App/>
   );
 }
