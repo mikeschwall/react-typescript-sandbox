7a5b6ad180eb4c569391b2cb09c51dd5def71960
Mike Schwall <mikschwall@gmail.com>
2026-01-09 11:30:36 -0600

    feature-commit

---
+++ src/store/apis/Todoapi.tsx
@@ -0,0 +1,33 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const todoApi = createApi({
+    reducerPath:"todos",
+    baseQuery:fetchBaseQuery({baseUrl:"https://jsonplaceholder.typicode.com/"}),
+    endpoints(builder) {
+        return {
+            fetchTodos: builder.query({
+                query: () => {
+                    return {
+                        method:"GET",
+                        url:"/todos"
+                    }
+                }
+            }),
+            sendPost: builder.mutation({
+                query: (user) => {
+                    return {
+                        method:"POST",
+                        url:"/posts",
+                        body: {
+                            title: user.title,
+                            body: user.body,
+                            userId: user.userId
+                        }
+                    }
+                }
+            })
+        }
+    }
+})
+
+export const {useFetchTodosQuery,useSendPostMutation} = todoApi;
