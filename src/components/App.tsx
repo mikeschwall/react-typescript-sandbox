d60a3d454fd7f9ecb7f37870e3e99c773cb2b675
Mike Schwall <mikschwall@gmail.com>
2026-05-06 08:33:31 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,11 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
-import TodoList from './TodoList';
-import AuthContext from '../context/Store';
 
 const App:React.FC = () => {
-    const {logStatus,logHandler} = useContext(AuthContext);
    
     return (
         <>
-        <button onClick={() => logHandler()}>{logStatus === true ? "log out" : "log in"}</button>
-        <TodoList/>
+        test
         </>
     )
 }
