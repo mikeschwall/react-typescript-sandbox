1fbac7f90e02e69d0b9eb0986fb7d95ac18eba69
Mike Schwall <mikschwall@gmail.com>
2026-07-20 09:14:28 -0500

    clean repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,11 @@
 import React, { useContext } from 'react';
 import { CssBaseline } from '@mui/material';
-import TodoList from './TodoList';
-import SongList from './Songlist';
 
 const App:React.FC = () => {
    
     return (
         <>
-        <SongList/>
-        <hr/>
-        <TodoList/>
+        test
         </>
     )
 }
